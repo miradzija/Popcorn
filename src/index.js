1be8e3dfb01607c27486b24edd5c7599d321a3d5
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-04-22 19:52:53 +0200

    star component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
@@ -9,6 +9,28 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating color="red" size={50} className="test" defaultRating={3} />
+    <StarRating color="blue" size={50} />
+    <Test />
   </React.StrictMode>
 );
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="green"
+        size={50}
+        maxRating={10}
+        onSetRating={setMovieRating}
+      />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
