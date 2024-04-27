2cffa955a1e069c03dfc2a0522d959b6bcd26252
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-04-27 14:04:31 +0200

    title

--- src/App-v1.js
+++ src/App-v1.js
@@ -332,6 +332,14 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.tilte = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
